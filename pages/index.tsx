d2f5a18e7990b6148ee2c62503428b51af9d9a8f
Mike Golus <michaelgolus@gmail.com>
2022-10-06 15:11:35 -0400

    Fixed typescript issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
 import { SliceZone } from '@prismicio/react'
+import { LandingPageDocument } from '../types.generated'
 
 import { createClient } from '../prismicio'
 import { components } from '../slices'
 
-const Page = ({ page }) => {
+interface PageProps {
+  page: LandingPageDocument
+  previewData: LandingPageDocument
+}
+
+const Page = ({ page }: PageProps) => {
   return <SliceZone slices={page.data.slices} components={components} />
 }
 
 export default Page
 
-export async function getStaticProps({ previewData }) {
+export async function getStaticProps({ previewData }: PageProps) {
   const client = createClient({ previewData })
 
   const page = await client.getSingle('landing_page')
