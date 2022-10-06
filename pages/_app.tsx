600dc2287d2d8d7840b15dd408638be61f2f942a
Mike Golus <michaelgolus@gmail.com>
2022-10-06 13:57:58 -0400

    Implemented Prismic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
+import Link from 'next/link'
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import { repositoryName } from '../prismicio'
+
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PrismicProvider
+      internalLinkComponent={({ href, ...props }) => (
+        <Link href={href}>
+          <a {...props} />
+        </Link>
+      )}
+    >
+        <PrismicPreview repositoryName={repositoryName}>
+          <Component {...pageProps} />
+        </PrismicPreview>
+      </PrismicProvider>
+    )
 }
 
 export default MyApp
